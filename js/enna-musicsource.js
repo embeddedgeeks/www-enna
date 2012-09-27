4773655fbe51511ab759522fc0a90203a9b04091
Raoul Hecky <raoul.hecky@gmail.com>
2012-09-27 16:15:48 +0200

    Better design

--- js/enna-musicsource.js
+++ js/enna-musicsource.js
@@ -8,7 +8,35 @@ $(document).ready(function()
                 success: function (data) {
                         $("#music_source_loading").fadeOut("normal");
                         for (var i = 0; i < data.result.length; i++) {
-                                $("#music_source_list").append("<b>" + data.result[i] + "</b><br/>");
+
+                                var row = '<tr>';
+                                row += '<td><b>' + data.result[i]['name'] + '</b></td>';
+                                row += '<td>' + data.result[i]['version'] + '</td>';
+                                row += '<td>' + data.result[i]['ip'] + '</td>';
+                                row += '<td><button class="btn" type="button">Connect</button></td>';
+                                row += '</tr>';
+
+                                $('#music_source_list > tbody:last').append(row);
+                        }
+                }
+        });
+
+        $.ajax({
+                url: 'action.php',
+                type: 'POST',
+                data: '{ "action": "music_source", "cmd": "current" }',
+                success: function (data) {
+                        $("#cmusic_source_loading").fadeOut("normal");
+                        for (var i = 0; i < data.result.length; i++) {
+
+                                var row = '<tr>';
+                                row += '<td><b>' + data.result[i]['name'] + '</b></td>';
+                                row += '<td>' + data.result[i]['version'] + '</td>';
+                                row += '<td>' + data.result[i]['ip'] + '</td>';
+                                row += '<td><button class="btn" type="button">Connect</button></td>';
+                                row += '</tr>';
+
+                                $('#music_source_list > tbody:last').append(row);
                         }
                 }
         });
--- music_source.php
+++ music_source.php
@@ -12,13 +12,66 @@
 <div id="main-title-help" class="hidden-phone">
 Select a valid Media Server where the box can connect and play your files.</div>
 
-<div class="round-box">
-        List of music sources
-        <img id="music_source_loading" alt="loading" src="img/ajax-loader.gif" />
+<div class="row-fluid round-box">
+        <div class="span4">
+                <div class="page-header">
+                        <h4>Current music source</h4>
+                </div>
 
-        <div id="music_source_list">
-
+                <img id="cmusic_source_loading" alt="loading" src="img/ajax-loader2.gif" />
+                <div id="music_source_current">
+                </div>
         </div>
+
+        <div class="span8">
+                <div class="page-header">
+                        <h4>List of music sources</h4>
+                </div>
+                <table id="music_source_list" class="table">
+                <thead>
+                <tr>
+                  <th style="width: 35%">Name</th>
+                  <th style="width: 20%">Version</th>
+                  <th style="width: 25%">IP address</th>
+                  <th style="width: 20%"></th>
+                </tr>
+                </thead>
+                <tbody></tbody>
+                </table>
+                <img id="music_source_loading" alt="loading" src="img/ajax-loader.gif" />
+
+                <table class="table">
+                <tbody>
+                        <tr>
+                        <td style="width: 30%">Manual Media Server IP Adress</td>
+                        <td style="width: 50%">
+                                <div class="input-prepend">
+                                <span class="add-on"><i class="icon-hdd"></i></span><input size="15" id="inputIP" type="text">
+                                </div>
+                        </td>
+                        <td style="width: 20%"><button class="btn" type="button">Connect</button></td>
+                        </tr>
+                </tbody>
+                </table>
+        </div>
+</div>
+
+<h2 id="main-title">Player Name</h2>
+<div id="main-title-help" class="hidden-phone">
+You can give this player a name that will be used to identify the player on the Media Server web pages.</div>
+
+<div class="row round-box">
+<form class="form-horizontal">
+        <div class="control-group">
+                <label class="control-label" for="inputName">Player Name</label>
+                <div class="controls">
+                        <input type="text" id="inputName" placeholder="Enna Player">
+                </div>
+                <div class="form-actions">
+                <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+        </div>
+</form>
 </div>
 
 </div>
